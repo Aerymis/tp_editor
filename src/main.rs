434475722e71a6c54412e805b97da33ba6955fc6
Donte Ravae <donte@curiosminds.io>
2022-10-21 14:41:40 -0400

    clean up main

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use crossterm::terminal;
-use tp_editor_core::{CleanUp, Editor};
+use tp_editor_core::{init_editor, clean_up};
 
 fn main() -> crossterm::Result<()> {
-    let _clean_up = CleanUp;
+    let _clean_up = clean_up();
     terminal::enable_raw_mode()?;
-    while Editor::new().run()? {}
-    Ok(())
+    init_editor()
 }
